40040518c8849b5c3a9ee270be43fd33fa4e3a97
andreutu <91362974+andreutu@users.noreply.github.com>
2025-04-03 00:23:37 +0300

    refactor(commands): remove dv command

--- server/vehicle/commands.ts
+++ server/vehicle/commands.ts
@@ -1,4 +1,4 @@
-import { addCommand, triggerClientCallback } from '@overextended/ox_lib/server';
+import { addCommand } from '@overextended/ox_lib/server';
 import { OxVehicle } from './class';
 import { sleep } from '@overextended/ox_lib';
 import { CreateVehicle } from 'vehicle';
@@ -46,34 +46,6 @@ addCommand<{ model: string; owner?: number }>(
         help: "Create a persistent vehicle owned by the target's active character.",
         optional: true,
       },
-    ],
-    restricted: 'group.admin',
-  }
-);
-
-addCommand<{ radius?: number; owned?: string }>(
-  'dv',
-  async (playerId, args, raw) => {
-    const ped = GetPlayerPed(playerId as any);
-
-    if (!args.radius) return DeleteCurrentVehicle(ped);
-
-    const vehicles = await triggerClientCallback<number[]>('ox:getNearbyVehicles', playerId, args.radius);
-
-    if (!vehicles) return;
-
-    vehicles.forEach((netId) => {
-      const vehicle = OxVehicle.getFromNetId(netId);
-
-      if (!vehicle) DeleteEntity(NetworkGetEntityFromNetworkId(netId));
-      else if (args.owned) vehicle.setStored('impound', true);
-    });
-  },
-  {
-    help: `Deletes your current vehicle, or any vehicles within range.`,
-    params: [
-      { name: 'radius', paramType: 'number', help: 'The radius to despawn vehicles (defaults to 2).', optional: true },
-      { name: 'owned', help: 'Include player-owned vehicles.', optional: true },
     ],
     restricted: 'group.admin',
   }
